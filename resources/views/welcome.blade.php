a05fa77b3708a5ece2bdfef841fac485db812652
Marcelo Maidana <marcelo@orchestrated.io>
2016-02-20 21:52:52 +1100

    Adding all project

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,12 @@
+@extends('_layout')
+
+@section('content')
+    <div class="content">
+        <div class="title">Write your name here</div>
+        <form method="POST">
+            {{ csrf_field() }}
+            <input type="text" name="name" />
+            <button type="submit">Send</button>
+        </form>
+    </div>
+@stop
